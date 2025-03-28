352bb4835a6ce9078b47e70b0fe934328be977b3
Nil Jay <njsfernandez@addu.edu.ph>
2025-03-29 00:40:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const app = express();
-
-const PORT = 5000;
-
-app.get('/', (req, res) => {
-    res.send('Hello, Docker!');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'Hello from Vercel!' });
 });
